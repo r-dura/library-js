c5a4264b6849eba6c108be3ecf0a50f48a539ba8
Raj Duraisamy <raj.duraisamy03@gmail.com>
2023-09-11 15:39:28 -0700

    Using the form to add a book's info now presents that book in a card format, using js to pull info from form

--- js.js
+++ js.js
@@ -4,9 +4,36 @@ function Book(title, author, pages, read) {
     this.title = title,
     this.author = author,
     this.pages = pages,
-    this.read = read,
+    this.read = read
 }
 
 function addBookToLibrary(book) {
-  myLibrary.push(book);
+    const newCard = document.createElement("div");
+    newCard.classList.add("card");
+    newCard.innerHTML = `
+        <h2>${book.title}</h2>
+        <p>Author: ${book.author}</p>
+        <p>Pages: ${book.pages}</p>
+        <p>Status: ${book.read}</p>
+    `;
+    const cards = document.getElementById('cards');
+    cards.appendChild(newCard);
 }
+
+document.getElementById('bookForm').addEventListener('submit', function(event) {
+    event.preventDefault(); // Prevent the default form submission behavior
+  
+    const title = document.getElementById('title').value;
+    const author = document.getElementById('author').value;
+    const numPages = document.getElementById('pages').value;
+    const status = document.getElementById('read').value;
+
+    const newBook = new Book(title, author, numPages, status);
+  
+    myLibrary.push(newBook);
+
+    this.reset();
+
+    addBookToLibrary(newBook);
+});
+
